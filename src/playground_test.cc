4faaf2312782a1abda7bbe6b2dec2271960c5f83
Chinmay Garde <chinmaygarde@gmail.com>
2024-03-01 01:19:07 -0800

    Make the build work on windows.

--- src/playground_test.cc
+++ src/playground_test.cc
@@ -5,30 +5,16 @@
 
 #include "GLFW/glfw3.h"
 #include "fml/logging.h"
-#include "fml/native_library.h"
 
 namespace one::testing {
 
-static void InitGLFWVulkanOnce() {
+static void InitGLFWOnce() {
   static std::once_flag gOnce;
-  std::call_once(gOnce, []() {
-    static auto library =
-        fml::NativeLibrary::Create("/usr/local/lib/libvulkan.dylib");
-    FML_CHECK(library) << "Must be able to find the Vulkan implementation.";
-    auto addr = library->ResolveFunction<PFN_vkGetInstanceProcAddr>(
-        "vkGetInstanceProcAddr");
-    FML_CHECK(addr.has_value())
-        << "Must be able to find the instance proc address.";
-    ::glfwSetErrorCallback([](int code, const char* message) -> void {
-      FML_LOG(ERROR) << "GLFW Error " << code << ": " << message;
-    });
-    ::glfwInitVulkanLoader(addr.value());
-    FML_CHECK(::glfwInit() == GLFW_TRUE);
-  });
+  std::call_once(gOnce, []() { FML_CHECK(::glfwInit() == GLFW_TRUE); });
 }
 
 PlaygroundTest::PlaygroundTest() {
-  InitGLFWVulkanOnce();
+  InitGLFWOnce();
   FML_CHECK(::glfwVulkanSupported())
       << "Vulkan must be supported on this platform";
   ::glfwDefaultWindowHints();
